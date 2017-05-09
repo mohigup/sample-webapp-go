fae9e8324ff5e6f0c627d23bc21a6f1f2a7ab14b
Mohit Gupta <gupta.moh@husky.neu.edu>
2017-05-08 22:56:56 -0400

    raw html to data driven htmls, added handlers

--- server/server.go
+++ server/server.go
@@ -2,10 +2,40 @@ package main
 
 import (
 	"net/http"
-
+	"html/template"
+	"log"
 )
 
 func main(){
 
-	http.ListenAndServe(":8000",http.FileServer(http.Dir("public")))
+	templates := populateTemplates()
+	// handle every URL
+	http.HandleFunc("/",func(w http.ResponseWriter, r *http.Request){
+		// slice slash
+		requestedFile := r.URL.Path[1:]
+		// get name of templates
+		t := templates.Lookup(requestedFile + ".html")
+		// error handling
+		if t!=nil{
+			err := t.Execute(w,nil)
+			if err != nil{
+				log.Println(err)
+			}
+		}
+	})
+
+	http.Handle("/img/",http.FileServer(http.Dir("public")))
+	http.Handle("/css/",http.FileServer(http.Dir("public")))
+	http.ListenAndServe(":8000",nil)
+}
+
+func populateTemplates() *template.Template{
+	// container for all templates
+	result := template.New("templates")
+	// path for templates
+	const BasePath  = "templates"
+	// parse templates for result, handles error
+	// any file ending with .html will be parsed
+	template.Must(result.ParseGlob(BasePath+"/*.html"))
+	return result
 }
